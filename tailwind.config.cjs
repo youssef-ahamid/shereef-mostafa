e180341ca486d1b570cb06b6a8a3bb75d8218820
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-22 16:55:56 +0200

    add fonts to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,12 @@ module.exports = {
         primary: "#E1E2EF",
         secondary: "#02020A",
         error: "#D0342C"
+      },
+      fontFamily: {
+        cursive: ['Homemade Apple', 'cursive'],
+        body: ['Josefin Slab', 'serif'],
+        mono: ['Fira Code', 'monospace'],
+        yes: ['Montagu Slab', 'serif']
       }
     },
   },
