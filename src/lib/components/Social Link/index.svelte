b1688937afcbfd58813a07efeb03a60b3b1f8177
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-23 01:46:14 +0200

    🦴  social links

---
+++ src/lib/components/Social Link/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  /* props */
+  export let link = '' // *, url
+  export let icon = null // *, icon component
+  export let redirect = true // *, false
+  export let className = '' // *, custom wrapper classes
+  
+  /* styles */
+  import { classes } from './styles'
+</script>
+
+<a href={link} class={classes.link + className} target:_blank={redirect}>
+  <svelte:component this={icon} />
+</a>
---
+++ src/lib/components/Social Link/styles.js
@@ -0,0 +1,3 @@
+export let classes = {
+  link: 'block w-8 h-8 mx-1 md:w-12 md:h-12 md:mx-2',
+}
