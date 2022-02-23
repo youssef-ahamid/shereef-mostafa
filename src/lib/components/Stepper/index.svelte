2d96febe5203b47f0f6172e09344a51ba158adf1
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-23 03:25:39 +0200

    🦴  Stepper

---
+++ src/lib/components/Stepper/index.svelte
@@ -0,0 +1,22 @@
+<script>
+  /* props */
+  export let steps = 0 // *, number of steps
+  export let active = 0 // *, current step
+  export let direction = 'vertical' // *, horizontal
+  export let className = '' // *, custom wrapper classes
+  
+  /* styles */
+  import { config, classes } from './styles'
+  config({ direction })
+
+  /* events */
+  import { createEventDispatcher } from 'svelte/internal'
+  const dispatch = createEventDispatcher()
+  const click = (id) => dispatch('click', id)
+</script>
+
+<div class={classes.stepper + className}>
+  {#each [...Array(steps).keys()] as step}
+    <span class="{classes.step} {step === active? classes.active: ''}" on:click={() => {click(step)}}></span>
+  {/each}
+</div>
---
+++ src/lib/components/Stepper/styles.js
@@ -0,0 +1,32 @@
+import { serializeClasses } from '$lib/helpers'
+
+const stepperStyles = [
+  { classes: 'flex' },
+  { direction: 'vertical', classes: 'flex-col' },
+  { direction: 'horizontal', classes: 'flex-row' },
+]
+
+const stepStyles = [
+  { classes: 'rounded-full transition duration-300 ease-out bg-primary bg-opacity-50 m-1' },
+  { direction: 'vertical', classes: 'w-1 min-h-[35px]' },
+  { direction: 'horizontal', classes: 'h-1 min-w-[35px]' },
+]
+
+const activeStyles = 'grow bg-opacity-100'
+
+let options
+const isActiveClass = style => style.direction === options.direction || !style.direction
+
+export let classes = {}
+const getClasses = () => {
+  classes = {
+    stepper: serializeClasses(stepperStyles, isActiveClass),
+    step: serializeClasses(stepStyles, isActiveClass),
+    active: activeStyles,
+  }
+}
+
+export const config = props => {
+  options = props
+  getClasses()
+}
