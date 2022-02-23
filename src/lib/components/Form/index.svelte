65fab1bf4684154845e38a2a34aa187ec2664256
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-24 01:09:20 +0200

    🦴 form skeleton

---
+++ src/lib/components/Form/index.svelte
@@ -0,0 +1,60 @@
+<script>
+  /* components */
+  import Button from '$lib/components/Button/index.svelte'
+
+  /* props */
+  export let inputs = [] // *, array of inputs
+  export let button = null // *, action object
+  export let className = '' // *, custom wrapper classes
+
+  /* data */
+  $: errors = inputs.map(input => {
+    return input.error
+  })
+  $: error = errors.includes(true)
+
+  /* styles */
+  import { config } from './styles'
+  $: classes = config({ error })
+
+  /* events */
+  import { createEventDispatcher } from 'svelte/internal'
+  const dispatch = createEventDispatcher()
+  const submit = () => {
+    let data = inputs.map(input => {
+      input.validate()
+      if (input.error) error = true
+      
+      return {
+        value: input.data.value,
+        key: input.data.label,
+        error: input.error,
+      }
+    })
+    if (error) dispatch('error', data)
+    else dispatch('submit', data)
+  }
+</script>
+
+<form
+  on:submit|preventDefault={submit}
+  class={classes.form + className}
+>
+  {#each inputs as input}
+    <svelte:component
+      this={input.component}
+      {...input.data}
+      bind:value={input.data.value}
+      bind:validate={input.validate}
+      on:invalid={() => {
+        input.error = true
+      }}
+      on:valid={() => {
+        input.error = false
+      }}
+    />
+  {/each}
+  {#if !!button}
+    <Button {...button} />
+  {/if}
+</form>
