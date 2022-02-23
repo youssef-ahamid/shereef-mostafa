6bedf916af61e7142721abb7af102e1269a26ead
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-23 15:26:22 +0200

    🦴 input skeleton

---
+++ src/lib/components/Text Input/index.svelte
@@ -0,0 +1,60 @@
+<script>
+  /* helpers */
+  import { assert } from "$lib/helpers";
+
+  /* props */
+  export let type = "text"; // *, text area
+  export let trim = false; // *, true
+  export let long = false; // *, true
+  export let validations = null; // *, array of validation objects
+  export let className = ""; // *, custom wrapper classes
+  export let label = ""; // *, label text
+  export let placeholder = ""; // *, placeholder text
+  export let value = ""; // *, bound value
+
+  /* data */
+  let clean = true;
+  export const validate = () => {
+    clean = assert(value, validations).success;
+    if (clean) valid();
+    else invalid();
+  };
+
+  /* styles */
+  import { config, classes } from "./styles";
+  $: config({ type, trim, long, clean });
+
+  /* events */
+  import { createEventDispatcher } from "svelte/internal";
+  const dispatch = createEventDispatcher();
+  const valid = () => dispatch("valid", value);
+  const invalid = () => dispatch("invalid", value);
+  const blur = () => {
+    validate();
+    dispatch("blur", value);
+  };
+  const focus = () => dispatch("focus", value);
+</script>
+
+<label class={classes.label + className} for={label}>
+  {label}
+  {#if type == "text"}
+    <input
+      bind:value
+      {placeholder}
+      name={label}
+      class={classes.input}
+      on:focus={focus}
+      on:blur={blur}
+    />
+  {:else if type == "text area"}
+    <textarea
+      bind:value
+      {placeholder}
+      name={label}
+      class={classes.input}
+      on:focus={focus}
+      on:blur={blur}
+    />
+  {/if}
+</label>
