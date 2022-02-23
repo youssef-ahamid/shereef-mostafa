544f5648c86823e160bfe32f33a41c5677ddec41
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-23 16:50:16 +0200

    ♻️  reactive classes

--- src/lib/components/Scroll Control/index.svelte
+++ src/lib/components/Scroll Control/index.svelte
@@ -2,32 +2,38 @@
   /* props */
   export let indicator = true // *, false
   export let timeout = 1500 // *, scroll timeout in ms
+  export let hijacked = true // *, false
 
   /* data */
   let scrollAllowed = true;
+  let direction = "up";
   
   /* styles */
-  import { config, classes } from './styles'
-  $: config({ scrollAllowed })
+  import { config } from './styles'
+  $: classes = config({ scrollAllowed, direction })
 
   /* events */
   import { createEventDispatcher } from 'svelte/internal'
   const dispatch = createEventDispatcher()
   const handleScroll = (e) => {
+    if (hijacked) e.preventDefault()
+    
     if (!scrollAllowed) return
-
-    let up = (e.wheelDelta && e.wheelDelta > 0) || (e.deltaY && e.deltaY < 0)
-    if (up) dispatch('up')
-    else dispatch('down')
+    
     scrollAllowed = false
     setTimeout(() => { 
       scrollAllowed = true 
     }, timeout)
+
+    let up = (e.wheelDelta && e.wheelDelta > 0) || (e.deltaY && e.deltaY < 0)
+    if (up) direction = "up"
+    else  direction = "down"
+    dispatch(direction)
   }
 
 </script>
 
-<svelte:window on:scroll|preventDefault={handleScroll} />
+<svelte:window on:mousewheel|nonpassive={handleScroll} on:scroll|nonpassive={handleScroll} />
 
 {#if indicator }
   <div class={classes.control}>
