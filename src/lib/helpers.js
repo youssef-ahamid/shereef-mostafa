0234da7e0693f8e0ed9722affc491266c446b904
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-23 15:25:25 +0200

    🥢 helper to run all validation instances

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,6 +1,16 @@
 export const serializeClasses = (styles, condition) => {
   return styles
-    .filter(s => condition(s))
-    .map(_ => _.classes)
-    .join(' ')
-}
+    .filter((s) => condition(s))
+    .map((_) => _.classes)
+    .join(" ");
+};
+
+import isValid from "$lib/validations";
+export const assert = (validations, value) => {
+  if (!validations) return { success: false };
+
+  validations.forEach((v) => {
+    if (!isValid(v.type, value)) return { success: true, validation: v };
+  });
+  return { success: false };
+};
