04fff391ee21028f109c8be5689cdc35d450a406
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-24 09:11:01 +0200

    add wiggle animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,20 @@ module.exports = {
         mono: ['Fira Code', 'monospace'],
         yes: ['Montagu Slab', 'serif'],
       },
+      keyframes: {
+        wiggle: {
+          '0%, 50%, 100%': { transform: 'translateX(0)' },
+          '60%': { transform: 'translateX(10px)' },
+          '70%': { transform: 'translateX(-10px)' },
+          '80%': { transform: 'translateX(10px)' },
+          '85%': { transform: 'translateX(-5px)' },
+          '90%': { transform: 'translateX(5px)' },
+          '95%': { transform: 'translateX(-5px)' },
+        }
+      },
+      animation: {
+        'wiggle': 'wiggle 1s linear forwards',
+      }
     },
   },
   plugins: [require('@tailwindcss/typography')],
