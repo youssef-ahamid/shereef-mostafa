c0c93a697ac305126eae8cdb8797a82ee402dfee
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-24 15:41:35 +0200

    🦴  thumbnail skeleton

--- src/lib/components/Thumbnail/index.svelte
+++ src/lib/components/Thumbnail/index.svelte
@@ -1,25 +1,26 @@
 <script>
+  /* components */
+  import CustomCursor from '../Custom Cursor/index.svelte'
+
   /* props */
-  export let src = ""; // *, image url
-  export let alt = ""; // *, image alt
-  export let fade = "none"; // *, small, medium, large
-  export let cursor = null; // *, cursor component
-  export let className = ""; // *, custom wrapper classes
+  export let src = '' // *, image url
+  export let alt = '' // *, image alt
+  export let fade = 'none' // *, small, medium, large
+  export let cursor = null // *, cursor icon
+  export let className = '' // *, custom wrapper classes
 
   /* styles */
-  import { config } from "./styles";
-  $: classes = config({ fade });
+  import { config } from './styles'
+  $: classes = config({ fade })
 </script>
 
-<img
-  on:click
-  on:mouseenter
-  on:mouseleave
-  class={classes.thumbnail + className}
-  {src}
-  {alt}
-/>
-
-{#if cursor }
-    <svelte:component this={cursor} />
-{/if}
+<CustomCursor icon={cursor}>
+  <img
+    on:click
+    on:mouseenter
+    on:mouseleave
+    class={`${classes.thumbnail} ${className}`}
+    {src}
+    {alt}
+  />
+</CustomCursor>
