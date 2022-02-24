8e1c3ec6de29b27d15e4b579c3370b938080c9ea
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-24 15:36:41 +0200

    🦴  custom cursor

---
+++ src/lib/components/Custom Cursor/index.svelte
@@ -0,0 +1,24 @@
+<script>
+  /* props */
+  export let icon // icon component to replace cursor
+
+  /* actions */
+  import { mousify } from '$lib/actions'
+
+  /* styles */
+  import { classes } from './styles'
+</script>
+
+<span
+  on:click
+  on:mouseenter
+  on:mouseleave
+  class={classes.cursorWrapper}
+>
+  <slot />
+</span>
+<span use:mousify class={classes.cursor}>
+  <svelte:component
+    this={icon}
+  />
+</span>
