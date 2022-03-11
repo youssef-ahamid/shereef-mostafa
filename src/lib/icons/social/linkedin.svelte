1937a12afff510c71785f3e1910c8992f02a2a06
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-03-11 18:59:07 +0200

    remove linkedin width & height

--- src/lib/icons/social/linkedin.svelte
+++ src/lib/icons/social/linkedin.svelte
@@ -6,8 +6,6 @@
   xmlns="http://www.w3.org/2000/svg"
   x="0px"
   y="0px"
-  width="50"
-  height="50"
   viewBox="0 0 30 30"
   class={`hover:text-[#0e76a8] transition duration-300 ease-out fill-current w-full h-auto ${className}`}
 >
