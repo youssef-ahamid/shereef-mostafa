aa98b0e7ab1d84439b0155bd04cc11f52682b40a
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-03-15 20:46:46 +0200

    feature skeleton

---
+++ src/lib/components/Feature/index.svelte
@@ -0,0 +1,19 @@
+<script>
+  /* props */
+  export let reverse = false // *, true
+  export let className = '' // *, custom wrapper classes
+
+  /* styles */
+  import { config } from './styles'
+  $: classes = config({ reverse })
+</script>
+
+<div class={`${classes.feature} ${className}`}>
+  <div class={classes.left}><slot name="image" /></div>
+  <div class={classes.right}>
+    <span class={classes.image}><slot name="image" /></span>
+    <h2><slot name="title" /></h2>
+    <p><slot name="body" /></p>
+    <div><slot name="cta" /></div>
+  </div>
+</div>
