9e13965698ea02958f0a0476ab07fd7c286b9133
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-03-19 08:21:29 +0200

    image component

---
+++ src/lib/components/Image/index.svelte
@@ -0,0 +1,20 @@
+<script>
+    /* props */
+    export let type = 'custom'
+    export let src = ''
+    export let alt = ''
+    export let className = '' // *, custom wrapper classes
+
+    /* styles */
+    import { stylus } from "$lib/helpers";
+    import { imgWrapper, imgOverlay, img } from './styles'
+    
+    let wrapper = stylus(imgWrapper({ type }))
+    let image = stylus(imgOverlay({ type }))
+    let overlay = stylus(img({ type, src }))
+</script>
+
+<div class={`${wrapper.classes} ${className}`} style={wrapper.styles}>
+    <img {src} {alt} class={image.classes} style={image.styles} />
+    <div class={overlay.classes} style={overlay.styles}></div>
+</div>
---
+++ src/lib/components/Image/styles.js
@@ -0,0 +1,23 @@
+export const imgWrapper = (props) => {
+  return [
+    { classes: "", styles: "", on: props.type == "custom" },
+    { classes: "", styles: "", on: props.type == "card" },
+    { classes: "", styles: "", on: props.type == "overlay" },
+  ];
+};
+
+export const imgOverlay = (props) => {
+  return [
+    { classes: "", styles: "", on: props.type == "custom" },
+    { classes: "", styles: "", on: props.type == "card" },
+    { classes: "", styles: "", on: props.type == "overlay" },
+  ];
+};
+
+export const img = (props) => {
+  return [
+    { classes: "", styles: "", on: props.type == "custom" },
+    { classes: "", styles: "", on: props.type == "card" },
+    { classes: "", styles: "", on: props.type == "overlay" },
+  ];
+};
