7abc941adf4c3fdf69e4263a55bbbd6cd062d582
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-03-19 08:22:49 +0200

    tex input+ fix typography

--- src/lib/components/Text Input/styles.js
+++ src/lib/components/Text Input/styles.js
@@ -13,7 +13,7 @@ export let input = options => {
   return [
     {
       classes:
-        'outline-none appearance-none px-1 py-2 transition duration-300 ease-out border-b-4 bg-primary bg-opacity-5 focus:bg-opacity-25 text-primary focus:text-accent rounded min-w-[180px]',
+        'outline-none appearance-none px-1 py-2 transition duration-300 ease-out border-b-4 bg-primary bg-opacity-5 focus:bg-opacity-25 text-primary focus:text-accent rounded min-w-[160px] w-full',
     },
     {
       on: [options.clean, false],
@@ -25,7 +25,7 @@ export let input = options => {
     },
     {
       on: [options.type, 'text area'],
-      classes: 'h-24 resize-none'
+      classes: 'h-24  min-w-[160px] resize-none'
     }
   ]
 }
