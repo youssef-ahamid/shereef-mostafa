1e3c30db8ae3e6f2fe3588067e5f951d8ca3755d
Youssef Abdelhamid <ya1027@nyu.edu>
2022-03-27 23:05:25 +0200

    improve transition

--- src/lib/components/Page Transition/index.svelte
+++ src/lib/components/Page Transition/index.svelte
@@ -28,34 +28,37 @@
 {#if !isPageLoaded || updating || start}
   <div
     class="fixed w-screen h-screen inset-0 z-[9999] bg-primary flex flex-col items-center justify-center"
-    in:fade={{ duration: 300 }}
-    out:fade={{ duration: 500, delay: 1400 }}
+    in:slide={{ duration: 300 }}
+    out:slide={{ duration: 500, delay: 1400 }}
   >
     <div class="flex justify-center items-center">
       <span
         class="text-secondary py-2 w-12 md:w-32 mr-2 md:mr-4"
-        in:slide={{ duration:200, delay:400}}
+        in:slide={{ duration: 200, delay: 400 }}
         out:fade={{ duration: 300, delay: 600 }}
       >
         <Logo innerCircle="animate-pulse" />
       </span>
       <h1
         class="text-secondary text-center py-4"
-        in:slide={{ duration:200, delay:400}}
+        in:slide={{ duration: 200, delay: 400 }}
         out:fade={{ duration: 300, delay: 500 }}
       >
         shereef mostafa
       </h1>
     </div>
     <span
-    class="rounded-full border-x-2 border-secondary animate-spin w-12 h-12 bg-transparent pb-12"
-    transition:slide={{ duration: 400, delay: 200 }}
+      class="rounded-full border-x-2 border-secondary animate-spin w-12 h-12 bg-transparent pb-12"
+      transition:slide={{ duration: 400, delay: 200 }}
     />
-    {#if !start }
-        <h2 class="text-secondary" transition:fade={{ duration: 500 }}>
+    <div
+      class="h-4 w-full flex justify-center transition duration-300 ease-out"
+    >
+      <h2
+        class="text-secondary text-center mx-auto max-w-[90%] md:max-w-md"
+      >
         <slot />
-    </h2>
-    {/if}
+      </h2>
+    </div>
   </div>
 {/if}
-
