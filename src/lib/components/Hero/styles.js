e04c8545e052000fd682f3ed2097896e3230129c
Youssef Abdelhamid <ya1027@nyu.edu>
2022-03-28 12:31:51 +0200

    remove logo link on mobile

--- src/lib/components/Hero/styles.js
+++ src/lib/components/Hero/styles.js
@@ -19,8 +19,8 @@ export let cta = options => {
   ]
 }
 
-export let logoWrapper = 'z-[980] absolute top-5 left-8 w-40 h-40 group'
-export let logo = 'hidden md:block overflow-visible ml-[5%] py-4 md:w-36 md:h-36 text-primary group-hover:text-accent origin-center'
+export let logoWrapper = 'z-[980] hidden md:block absolute top-5 left-8 w-40 h-40 group'
+export let logo = 'block overflow-visible ml-[5%] py-4 md:w-36 md:h-36 text-primary group-hover:text-accent origin-center'
 export let logoInner = 'group-hover:scale-95 group-active:scale-90 origin-center'
 export let logoOuter = 'group-hover:scale-105 origin-center group-active:scale-110'
 
