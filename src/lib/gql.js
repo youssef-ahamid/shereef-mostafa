29f3781fa8acbc2507e499207321fb09a9c2c2de
Youssef Abdelhamid <ya1027@nyu.edu>
2022-03-30 12:25:42 +0200

    change ordering + update nav styles

--- src/lib/gql.js
+++ src/lib/gql.js
@@ -4,23 +4,6 @@ export const graphcms = new GraphQLClient(
   import.meta.env.VITE_GRAPHCMS_URL
 )
 
-export const sendMessage = async message => {
-  const mutation = gql`
-    mutation {
-        createMessage(data: { name: "${message.name}", email: "${
-    message.email
-  }", message: "${message.message}", phone: "${
-    message.phone || ''
-  }", company: "${message.company || ''}" }) {
-          id
-          name
-        }
-      }
-  `
-  const data = await graphcms.request(mutation, message)
-  return data
-}
-
 export async function getBios() {
   const query = gql`
     {
@@ -43,7 +26,7 @@ export async function getBios() {
 export async function getProjects(limit = 15) {
   const query = gql`
     {
-      projects(first: ${limit}) {
+      projects(orderBy: id_DESC, first: ${limit}) {
         thumbnail {
           url(
             transformation: { document: { output: { format: jpg } } }
